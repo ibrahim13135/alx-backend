32bf3df8ce6bde3aad2c66328643051c99e2ebc4
Hima <abrahimmahmoud5454@gmail.com>
2024-06-23 23:06:37 +0300

    task 1

--- 0x00-pagination/test.py
+++ 0x00-pagination/test.py
@@ -1,30 +1,47 @@
-import math
+# import math
 
 
-total_items = int(input("Enter the size: "))   # 100  
+# total_items = int(input("Enter the size of index: "))   # 100  
 
-data = list(range(1, total_items + 1))
+# data = list(range(1, total_items + 1))
 
-page_size = int(input("Enter the page size: "))
+# page_size = int(input("Enter the page size: "))
 
-page_number_start = int(input("Enter the page number: "))
+# page_number_start = int(input("Enter the page number: "))
 
-total_pages = math.ceil(total_items / page_size)
+# total_pages = math.ceil(total_items / page_size)
 
-start = (page_number_start - 1) * page_size 
+# start = (page_number_start - 1) * page_size  #
 
-end = start + page_size
+# end = start + page_size
 
-specif_page = math.ceil(page_number_start / page_size)
+# specif_page = math.ceil(page_number_start / page_size)
 
-page_data = data[start: end]
+# page_data = data[start: end]
 
 
 
-print("Total items:", total_items)
-print("Total pages:", total_pages)
-print("Start index:", start)
-print("End index:", end)
-print("page_content", page_data)
+# print("Total items:", total_items)
+# print("Total pages:", total_pages)
+# print("Start index:", start)
+# print("End index:", end)
+# print("page_content", page_data)
 
-print("size for page : ",len(page_data))
+# print("size for page : ",len(page_data))
+
+
+
+
+
+
+# def calculate_average(numbers):
+#     assert len(numbers) > 0, "List must not be empty"
+#     total = sum(numbers)
+#     average = total / len(numbers)
+#     return average
+
+# data = []
+# result = calculate_average(data)
+
+
+# print(result)
